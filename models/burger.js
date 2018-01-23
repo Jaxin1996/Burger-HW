4544d77213d931a0c7d19ffdfee8a0b811b8adab
Dominic San Filippo <jaxin@Dominics-MacBook-Pro.local>
2018-01-23 13:37:54 -0800

    added models, routes and fixed connection to all the files

--- models/burger.js
+++ models/burger.js
@@ -1,12 +1,30 @@
-module.exports = function(sequelize, DataTypes) {
-    var burgers = sequelize.define("burgers", {
-        burger_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          devoured: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-          }
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('burgers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      burger_name: {
+        type: Sequelize.STRING
+      },
+      devoured: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
     });
-}
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('burgers');
+  }
+};
--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-// Pull in required dependencies
+// Node Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var methodOverride = require('method-override')
 
+// Open Server
 var port = process.env.PORT || 3000;
-
+// Set up Express
 var app = express();
 
-// Serve static content for the app from the 'public' directory
+//Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
+// app.use(express.static('public'));
 
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
-
-// Set Handlebars as the view engine
+// Handlebars
 var exphbs = require('express-handlebars');
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Import routes and give the server access to them
-var routes = require('./controllers/controller.js');
 
-app.use('/', routes);
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
 
-app.listen(port);
+
+app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
